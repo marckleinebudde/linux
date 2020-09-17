8a8cb00f7f9868b966ab838c5143ee7f1d4acf46
Marc Kleine-Budde <mkl@pengutronix.de>
2020-09-17 22:24:20 +0200

    spi: spi-imx: spi_imx_transfer(): add support for effective_speed_hz
    This patch implementes the reporting of the effectivly used speed_hz for the
    transfer by setting tfr->effective_speed_hz.
    
    See the following patch, which adds this feature to the SPI core for more
    information:
    
        5d7e2b5ed585 spi: core: allow reporting the effectivly used speed_hz for a transfer
    
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Link: https://lore.kernel.org/r/20200917202420.1914104-1-mkl@pengutronix.de
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit bf253e6bf6b876a4ce74db7dcf8a13b80d84aa5f)
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/spi/spi-imx.c
+++ drivers/spi/spi-imx.c
@@ -1312,6 +1312,8 @@ static int spi_imx_transfer(struct spi_device *spi,
 {
 	struct spi_imx_data *spi_imx = spi_master_get_devdata(spi->master);
 
+	transfer->effective_speed_hz = spi_imx->spi_bus_clk;
+
 	if (spi_imx->usedma)
 		return spi_imx_dma_transfer(spi_imx, transfer);
 	else
