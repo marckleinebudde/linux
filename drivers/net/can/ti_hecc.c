f48fc46d2ea72c422ea4cb994fc4ba7edb1a0804
Oliver Hartkopp <socketcan@hartkopp.net>
2020-11-10 11:18:46 +0100

    can: rename get_can_dlc() macro with can_cc_dlc2len()
    The get_can_dlc() macro is used to ensure the payload length information of
    the Classical CAN frame to be max 8 bytes (the CAN_MAX_DLEN).
    
    Rename the macro and use the correct constant in preparation of the len/dlc
    cleanup for Classical CAN frames.
    
    Signed-off-by: Oliver Hartkopp <socketcan@hartkopp.net>
    Link: https://lore.kernel.org/r/20201110101852.1973-3-socketcan@hartkopp.net
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    (cherry picked from commit 69d98969a0540039fc04e0f22bbe9f41b0a13d66)
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/ti_hecc.c
+++ drivers/net/can/ti_hecc.c
@@ -566,7 +566,7 @@ static int ti_hecc_rx_pkt(struct ti_hecc_priv *priv, int mbxno)
 	data = hecc_read_mbx(priv, mbxno, HECC_CANMCF);
 	if (data & HECC_CANMCF_RTR)
 		cf->can_id |= CAN_RTR_FLAG;
-	cf->can_dlc = get_can_dlc(data & 0xF);
+	cf->can_dlc = can_cc_dlc2len(data & 0xF);
 	data = hecc_read_mbx(priv, mbxno, HECC_CANMDL);
 	*(__be32 *)(cf->data) = cpu_to_be32(data);
 	if (cf->can_dlc > 4) {
