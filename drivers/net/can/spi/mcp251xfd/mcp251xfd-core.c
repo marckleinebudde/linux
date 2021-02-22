5627c54e766c0d87a9cc5e7de51fc7b07581363d
Marc Kleine-Budde <mkl@pengutronix.de>
2021-02-22 20:46:06 +0100

    can: mcp251xfd: revert "can: mcp251xfd: add BQL support"
    In the following 4 patches
    
    | 99842c9685ab can: dev: can_rx_offload_get_echo_skb(): extend to return can frame length
    | 9420e1d495e2 can: dev: can_get_echo_skb(): extend to return can frame length
    | 1dcb6e57db83 can: dev: can_put_echo_skb(): extend to handle frame_len
    | f0ef72febc9a can: dev: extend struct can_skb_priv to hold CAN frame length
    
    the CAN echo SKB support was extended to hold the CAN frame
    length (which is the length of the CAN frame on the wire). It is meant
    as a helper for BQL support, to avoid the re-calculation of the frame
    length before sending it and on TX-completion.
    
    However if the CAN frame is send without the request to be looped back
    the SKB is discarded in can_put_echo_skb() and the subsequent
    can_get_echo_skb() and can_rx_offload_get_echo_skb() return 0 for the
    CAN frame length. This results in BQL stalling the TX queue after a
    few packages.
    
    Until the BQL helpers can_get_echo_skb() and
    can_rx_offload_get_echo_skb() are fixed, revert the BQL support for
    the mcp251xfd driver.
    
    This reverts commit 4162e18e949ba520d5116ac0323500355479a00e.
    
    Fixes: 4162e18e949b ("can: mcp251xfd: add BQL support")
    Cc: Manivannan Sadhasivam <mani@kernel.org>
    Cc: Thomas Kopp <thomas.kopp@microchip.com>
    Link: https://lore.kernel.org/r/20210228083347.28580-1-mkl@pengutronix.de
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    (cherry picked from commit 2afe72ead5ab672c8012bda83cbe65f8145568e0)
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/spi/mcp251xfd/mcp251xfd-core.c
+++ drivers/net/can/spi/mcp251xfd/mcp251xfd-core.c
@@ -336,8 +336,6 @@ static void mcp251xfd_ring_init(struct mcp251xfd_priv *priv)
 	u8 len;
 	int i, j;
 
-	netdev_reset_queue(priv->ndev);
-
 	/* TEF */
 	tef_ring = priv->tef;
 	tef_ring->head = 0;
@@ -1250,8 +1248,7 @@ mcp251xfd_handle_tefif_recover(const struct mcp251xfd_priv *priv, const u32 seq)
 
 static int
 mcp251xfd_handle_tefif_one(struct mcp251xfd_priv *priv,
-			   const struct mcp251xfd_hw_tef_obj *hw_tef_obj,
-			   unsigned int *frame_len_ptr)
+			   const struct mcp251xfd_hw_tef_obj *hw_tef_obj)
 {
 	struct net_device_stats *stats = &priv->ndev->stats;
 	u32 seq, seq_masked, tef_tail_masked;
@@ -1273,8 +1270,7 @@ mcp251xfd_handle_tefif_one(struct mcp251xfd_priv *priv,
 	stats->tx_bytes +=
 		can_rx_offload_get_echo_skb(&priv->offload,
 					    mcp251xfd_get_tef_tail(priv),
-					    hw_tef_obj->ts,
-					    frame_len_ptr);
+					    hw_tef_obj->ts, NULL);
 	stats->tx_packets++;
 	priv->tef->tail++;
 
@@ -1332,7 +1328,6 @@ mcp251xfd_tef_obj_read(const struct mcp251xfd_priv *priv,
 static int mcp251xfd_handle_tefif(struct mcp251xfd_priv *priv)
 {
 	struct mcp251xfd_hw_tef_obj hw_tef_obj[MCP251XFD_TX_OBJ_NUM_MAX];
-	unsigned int total_frame_len = 0;
 	u8 tef_tail, len, l;
 	int err, i;
 
@@ -1354,9 +1349,7 @@ static int mcp251xfd_handle_tefif(struct mcp251xfd_priv *priv)
 	}
 
 	for (i = 0; i < len; i++) {
-		unsigned int frame_len;
-
-		err = mcp251xfd_handle_tefif_one(priv, &hw_tef_obj[i], &frame_len);
+		err = mcp251xfd_handle_tefif_one(priv, &hw_tef_obj[i]);
 		/* -EAGAIN means the Sequence Number in the TEF
 		 * doesn't match our tef_tail. This can happen if we
 		 * read the TEF objects too early. Leave loop let the
@@ -1366,8 +1359,6 @@ static int mcp251xfd_handle_tefif(struct mcp251xfd_priv *priv)
 			goto out_netif_wake_queue;
 		if (err)
 			return err;
-
-		total_frame_len += frame_len;
 	}
 
  out_netif_wake_queue:
@@ -1398,7 +1389,6 @@ static int mcp251xfd_handle_tefif(struct mcp251xfd_priv *priv)
 			return err;
 
 		tx_ring->tail += len;
-		netdev_completed_queue(priv->ndev, len, total_frame_len);
 
 		err = mcp251xfd_check_tef_tail(priv);
 		if (err)
@@ -2444,7 +2434,6 @@ static netdev_tx_t mcp251xfd_start_xmit(struct sk_buff *skb,
 	struct mcp251xfd_priv *priv = netdev_priv(ndev);
 	struct mcp251xfd_tx_ring *tx_ring = priv->tx;
 	struct mcp251xfd_tx_obj *tx_obj;
-	unsigned int frame_len;
 	u8 tx_head;
 	int err;
 
@@ -2463,9 +2452,7 @@ static netdev_tx_t mcp251xfd_start_xmit(struct sk_buff *skb,
 	if (mcp251xfd_get_tx_free(tx_ring) == 0)
 		netif_stop_queue(ndev);
 
-	frame_len = can_skb_get_frame_len(skb);
-	can_put_echo_skb(skb, ndev, tx_head, frame_len);
-	netdev_sent_queue(priv->ndev, frame_len);
+	can_put_echo_skb(skb, ndev, tx_head, 0);
 
 	err = mcp251xfd_tx_obj_write(priv, tx_obj);
 	if (err)
