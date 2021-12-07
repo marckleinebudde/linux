ccf240e1b5225a61ea5261edbe1c229aa6b2cd6c
Vincent Mailhol <mailhol.vincent@wanadoo.fr>
2021-12-07 21:15:27 +0900

    can: do not increase rx statistics when generating a CAN rx error message frame
    The CAN error message frames (i.e. error skb) are an interface
    specific to socket CAN. The payload of the CAN error message frames
    does not correspond to any actual data sent on the wire. Only an error
    flag and a delimiter are transmitted when an error occurs (c.f. ISO
    11898-1 section 10.4.4.2 "Error flag").
    
    For this reason, it makes no sense to increment the rx_packets and
    rx_bytes fields of struct net_device_stats because no actual payload
    were transmitted on the wire.
    
    This patch fixes all the CAN drivers.
    
    Link: https://lore.kernel.org/all/20211207121531.42941-2-mailhol.vincent@wanadoo.fr
    CC: Marc Kleine-Budde <mkl@pengutronix.de>
    CC: Nicolas Ferre <nicolas.ferre@microchip.com>
    CC: Alexandre Belloni <alexandre.belloni@bootlin.com>
    CC: Ludovic Desroches <ludovic.desroches@microchip.com>
    CC: Chandrasekar Ramakrishnan <rcsekar@samsung.com>
    CC: Maxime Ripard <mripard@kernel.org>
    CC: Chen-Yu Tsai <wens@csie.org>
    CC: Jernej Skrabec <jernej.skrabec@gmail.com>
    CC: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
    CC: Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>
    CC: Michal Simek <michal.simek@xilinx.com>
    CC: Stephane Grosjean <s.grosjean@peak-system.com>
    Tested-by: Jimmy Assarsson <extja@kvaser.com> # kvaser
    Signed-off-by: Vincent Mailhol <mailhol.vincent@wanadoo.fr>
    Acked-by: Stefan Mätje <stefan.maetje@esd.eu> # esd_usb2
    Tested-by: Stefan Mätje <stefan.maetje@esd.eu> # esd_usb2
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    (cherry picked from commit 676068db69b847f06fe054fca15bf6b107bd24da)
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/m_can/m_can.c
+++ drivers/net/can/m_can/m_can.c
@@ -517,8 +517,6 @@ static int m_can_handle_lec_err(struct net_device *dev,
 		break;
 	}
 
-	stats->rx_packets++;
-	stats->rx_bytes += cf->len;
 	netif_receive_skb(skb);
 
 	return 1;
@@ -565,7 +563,6 @@ static int m_can_handle_state_change(struct net_device *dev,
 				     enum can_state new_state)
 {
 	struct m_can_priv *priv = netdev_priv(dev);
-	struct net_device_stats *stats = &dev->stats;
 	struct can_frame *cf;
 	struct sk_buff *skb;
 	struct can_berr_counter bec;
@@ -629,8 +626,6 @@ static int m_can_handle_state_change(struct net_device *dev,
 		break;
 	}
 
-	stats->rx_packets++;
-	stats->rx_bytes += cf->len;
 	netif_receive_skb(skb);
 
 	return 1;
